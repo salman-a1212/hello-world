b66b67a82ada38b6a8a99eff8cd8d7733afd3d9f
salman-a1212 <82266019+salman-a1212@users.noreply.github.com>
2022-01-20 12:46:58 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  document.body.style.backgroundColor = "rgb(162, 162, 170)";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <div className='container'>
+        <h1>This is a Hello World</h1>
+        <h2>
+          Made by <i>Salman Aslam</i>
+        </h2>
+        <h3>Para Heading</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          architecto mollitia, vel assumenda culpa deserunt labore, vitae iure
+          vero exercitationem similique incidunt? Assumenda, consequatur
+          pariatur ratione quisquam, quas suscipit veritatis iusto itaque enim
+          iure architecto quasi quam similique totam, blanditiis corrupti magnam
+          deserunt temporibus laborum! Beatae consectetur vitae tenetur
+          obcaecati cum hic vel possimus, maxime dolore exercitationem! Dolores,
+          aperiam sint aut maiores error maxime sunt voluptatum delectus earum
+          vitae recusandae nulla quaerat nobis architecto laudantium facilis
+          tempore quam dolore. Eligendi aliquid beatae, fuga hic velit
+          necessitatibus vero rerum tempora doloremque consequuntur ea explicabo
+          perspiciatis enim aperiam! Neque praesentium officia sapiente.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>Para Heading</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          architecto mollitia, vel assumenda culpa deserunt labore, vitae iure
+          vero exercitationem similique incidunt? Assumenda, consequatur
+          pariatur ratione quisquam, quas suscipit veritatis iusto itaque enim
+          iure architecto quasi quam similique totam, blanditiis corrupti magnam
+          deserunt temporibus laborum! Beatae consectetur vitae tenetur
+          obcaecati cum hic vel possimus, maxime dolore exercitationem! Dolores,
+          aperiam sint aut maiores error maxime sunt voluptatum delectus earum
+          vitae recusandae nulla quaerat nobis architecto laudantium facilis
+          tempore quam dolore. Eligendi aliquid beatae, fuga hic velit
+          necessitatibus vero rerum tempora doloremque consequuntur ea explicabo
+          perspiciatis enim aperiam! Neque praesentium officia sapiente.
+        </p>
+        <h3>Para Heading</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          architecto mollitia, vel assumenda culpa deserunt labore, vitae iure
+          vero exercitationem similique incidunt? Assumenda, consequatur
+          pariatur ratione quisquam, quas suscipit veritatis iusto itaque enim
+          iure architecto quasi quam similique totam, blanditiis corrupti magnam
+          deserunt temporibus laborum! Beatae consectetur vitae tenetur
+          obcaecati cum hic vel possimus, maxime dolore exercitationem! Dolores,
+          aperiam sint aut maiores error maxime sunt voluptatum delectus earum
+          vitae recusandae nulla quaerat nobis architecto laudantium facilis
+          tempore quam dolore. Eligendi aliquid beatae, fuga hic velit
+          necessitatibus vero rerum tempora doloremque consequuntur ea explicabo
+          perspiciatis enim aperiam! Neque praesentium officia sapiente.
+        </p>
+      </div>
     </div>
   );
 }
